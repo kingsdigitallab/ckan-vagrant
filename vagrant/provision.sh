2c6797b203802f69cea0b72ff0eaee1a37fdbff9
Miguel Vieira <jmvieira@gmail.com>
2018-03-22 17:06:24 +0000

    Updated the package provisioning script to work with the latest available version of CKAN.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,77 +1,100 @@
-echo "this shell script is going to setup a running ckan instance based on the CKAN 2.6.0 packages"
+echo
+echo "Installing CKAN from package"
 
-echo "switching the OS language"
+echo
+echo "- Set locale"
 locale-gen
-export LC_ALL="en_US.UTF-8"
-sudo locale-gen en_US.UTF-8
+export LC_ALL="en_GB.UTF-8"
+sudo locale-gen en_GB.UTF-8
 
-echo "updating the package manager"
-sudo apt-get update
+echo
+echo "1. Install the CKAN package"
+echo "- Update Ubuntu's package index"
+sudo apt update
+sudo apt-mark hold grub*
+sudo apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" dist-upgrade
 
-echo "installing dependencies available via apt-get"
-sudo apt-get install -y nginx apache2 libapache2-mod-wsgi libpq5
+echo
+echo "- Install the Ubuntu packages that CKAN requires"
+sudo apt install -y nginx apache2 libapache2-mod-wsgi libpq5 redis-server git-core
+# Apache needs to be stopped for nginx to run
+sudo service apache2 stop
+# nginx is a dependency of ckan
+sudo service nginx start
 
-echo "downloading the CKAN package"
-wget -q http://packaging.ckan.org/python-ckan_2.6.0_amd64.deb
+echo
+echo "- Enable wsgi module for apache"
+sudo a2enmod wsgi
 
-echo "installing the CKAN package"
-sudo dpkg -i python-ckan_2.6.0_amd64.deb
+echo
+echo "- Configuring packages"
+sudo dpkg --configure -a
 
-echo "Preventing NGINX from being started on a reboot"
-sudo update-rc.d -f nginx disable
+echo
+echo "- Download the CKAN package"
+wget -q http://packaging.ckan.org.s3-eu-west-1.amazonaws.com/python-ckan_2.7-xenial_amd64.deb
 
-echo "changing the apache configuration back to port 80"
-sudo cp /vagrant/vagrant/package_ports.conf /etc/apache2/ports.conf
-sudo cp /vagrant/vagrant/package_ckan_default.conf /etc/apache2/sites-available/ckan_default
-sudo service apache2 restart
+echo
+echo "- Install the CKAN package"
+sudo dpkg -i python-ckan_2.7-xenial_amd64.deb
 
-echo "install postgresql and jetty"
-sudo apt-get install -y postgresql solr-jetty openjdk-6-jdk
+echo
+echo "- Configuring packages"
+sudo dpkg --configure -a
 
-echo "copying jetty configuration"
-cp /vagrant/vagrant/jetty /etc/default/jetty
-sudo service jetty start
+echo
+echo "2. Install and configure PostgreSQL"
+echo "- Install PostgreSQL"
+sudo apt install -y postgresql
 
-echo "linking the solr schema file"
-sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/dutch_stop.txt /etc/solr/conf/dutch_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/english_stop.txt /etc/solr/conf/english_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/fr_elision.txt /etc/solr/conf/fr_elision.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/french_stop.txt /etc/solr/conf/french_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/german_stop.txt /etc/solr/conf/german_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/greek_stopwords.txt /etc/solr/conf/greek_stopwords.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/italian_stop.txt /etc/solr/conf/italian_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/polish_stop.txt /etc/solr/conf/polish_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/portuguese_stop.txt /etc/solr/conf/portuguese_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/romanian_stop.txt /etc/solr/conf/romanian_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/spanish_stop.txt /etc/solr/conf/spanish_stop.txt
-sudo ln -s /usr/lib/ckan/default/src/ckan/ckanext/multilingual/solr/schema.xml /etc/solr/conf/schema.xml
-# sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml
-
-sudo service jetty restart
-
-echo "create a CKAN database in postgresql"
+echo
+echo "- Create a database user"
 sudo -u postgres createuser -S -D -R ckan_default
 sudo -u postgres psql -c "ALTER USER ckan_default with password 'pass'"
+
+echo
+echo "- Create ckan database"
 sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
 
-echo "initialize CKAN database"
-cp /vagrant/vagrant/package_production.ini /etc/ckan/default/production.ini
+echo
+echo "3. Install and configure Solr"
+echo "- Install Solr"
+sudo apt install -y solr-jetty
+
+echo "- Edit Jetty configuration"
+sudo cp /vagrant/vagrant/jetty /etc/default/jetty8
+sudo service jetty8 restart
+
+echo
+echo "- Replace the default schema.xml file with a symlink to the CKAN schema"
+sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
+sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty8 restart
+
+echo
+echo "4. Update the configuration and initialize the database"
+echo "- Edit the CKAN configuration file"
+sudo cp /vagrant/vagrant/production.ini /etc/ckan/default/production.ini
+
+echo "- Initialize your CKAN database"
 sudo ckan db init
 
-echo "enabling filestore with local storage"
+echo "- Enable filestore with local storage"
 sudo mkdir -p /var/lib/ckan/default
 sudo chown www-data /var/lib/ckan/default
 sudo chmod u+rwx /var/lib/ckan/default
-sudo service apache2 restart
 
-echo "creating an admin user"
+echo
+echo "- Create an admin user"
 source /usr/lib/ckan/default/bin/activate
 cd /usr/lib/ckan/default/src/ckan
 paster --plugin=ckan user add admin email=admin@email.org password=pass -c /etc/ckan/default/production.ini
 paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/production.ini
 
-echo "loading some multilingual test data"
-paster --plugin=ckan create-test-data translations -c /etc/ckan/default/production.ini
+echo
+echo "- 5. Restart services"
+sudo service apache2 restart
+sudo service nginx restart
 
-echo "you should now have a running instance on http://ckan.lo"
+echo
+echo "Open http://localhost:9000/ in your web browser"
